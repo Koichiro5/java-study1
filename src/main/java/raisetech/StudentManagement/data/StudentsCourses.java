c080fbec1910720d1446b6b0e18d0dfbd6963930
Koichiro Ishikawa <koutomotaku5@gmail.com>
2024-11-05 20:37:15 +0900

    Merge remote-tracking branch 'origin/main'

--- src/main/java/raisetech/StudentManagement/data/StudentsCourses.java
+++ src/main/java/raisetech/StudentManagement/data/StudentsCourses.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class StudentCourses {
+public class StudentsCourses {
 
   private String id;
   private String studentId;
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -1,10 +1,13 @@
 package raisetech.StudentManagement.repository;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentCourses;
+import raisetech.StudentManagement.data.StudentsCourses;
 
 @Mapper
 public interface StudentRepository {
@@ -12,7 +15,31 @@ public interface StudentRepository {
   @Select("SELECT * FROM students")
   List<Student> search();
 
+  @Select("SELECT * FROM students WHERE id = #{id}")
+  Student searchStudent(String id);
+
   @Select("SELECT * FROM students_courses")
-  List<StudentCourses> searchCourses();
+  List<StudentsCourses> searchCoursesList();
+
+  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+  List<StudentsCourses> searchStudentsCourses(String studentId);
+
+  @Insert("INSERT INTO students(name, furigana, nickname, email, region, age, gender, remark, is_Deleted) "
+      + "VALUES (#{name}, #{furigana}, #{nickname}, #{email}, #{region}, #{age}, #{gender}, #{remark}, false)")
+  @Options(useGeneratedKeys = true,keyProperty = "id")
+  void registerStudent(Student student);
+
+  @Insert("INSERT INTO students_courses(student_id, course_name, course_start_date, course_end_date) "
+    + "VALUES(#{studentId}, #{courseName}, #{courseStartDate}, #{courseEndDate})")
+  @Options(useGeneratedKeys = true,keyProperty = "id")
+  void registerStudentCourses(StudentsCourses studentCourses);
+
+  @Update("UPDATE students SET name = #{name}, furigana = #{furigana}, nickname = #{nickname}, "
+      + "email = #{email}, region = #{region}, age = #{age}, gender = #{gender}, "
+      + "remark = #{remark}, is_Deleted = #{isDeleted} WHERE id = #{id}")
+  void updateStudent(Student student);
+
+  @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id}")
+  void updateStudentCourses(StudentsCourses studentCourses);
 
 }
