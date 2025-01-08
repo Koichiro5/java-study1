43880f83bef42e4f870a8629188e04430724d584
Koichiro Ishikawa <koutomotaku5@gmail.com>
2025-01-08 20:01:45 +0900

    課題21提出します。 リファクタリングを行いました。

--- src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
+++ src/main/java/raisetech/StudentManagement/controller/converter/StudentConverter.java
@@ -5,25 +5,35 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.data.StudentsCourse;
 import raisetech.StudentManagement.domain.StudentDetail;
 
+/**
+ * 受講背詳細を受講生や受講生コース情報、もしくはその逆の変換を行うコンバーターです。
+ */
 @Component
 public class StudentConverter {
-
-  public List<StudentDetail> convertStudentDetails(List<Student> students, List<StudentsCourses> studentCourses) {
+  /**
+   * 受講生に紐づく受講生コース情報をマッピングする。
+   * 受講生コース情報は受講生に対して複数存在するのでループを回して受講生詳細情報を組み立てる。
+   * @param students　受講生一覧
+   * @param studentCourses　受講生コース情報のリスト
+   * @return　受講生詳細情報のリスト
+   */
+  public List<StudentDetail> convertStudentDetails(List<Student> students,
+      List<StudentsCourse> studentCourses) {
     List<StudentDetail>studentDetails = new ArrayList<>();
-    for (Student student : students){
+    students.forEach(student ->{
       StudentDetail studentDetail = new StudentDetail();
       studentDetail.setStudent(student);
 
-      List<StudentsCourses> convertStudentCourses = studentCourses.stream()
+      List<StudentsCourse> convertStudentCourses = studentCourses.stream()
           .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
           .collect(Collectors.toList());
 
       studentDetail.setStudentCourses(convertStudentCourses);
       studentDetails.add(studentDetail);
-    };
+    });
     return studentDetails;
   }
 }
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -7,32 +7,55 @@ import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.data.StudentsCourse;
 
+/**
+ * 受講生テーブルと受講生コース情報テーブルと紐づくRepositoryです。
+ */
 @Mapper
 public interface StudentRepository {
-
-  @Select("SELECT * FROM students WHERE is_Deleted = false")
+  /**
+   * 受講生の全件検索を行います。
+   * @return　受講生一覧（全件）
+   */
+  @Select("SELECT * FROM students")
   List<Student> search();
-
+  /**
+   * 受講生の検索を行います。
+   * @param id　受講生ID
+   * @return　受講生
+   */
   @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(String id);
-
+  /**
+   * 受講生のコース情報の全件検索を行います。
+   * @return　受講生のコース情報（全件）
+   */
   @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchCoursesList();
-
+  List<StudentsCourse> searchStudentsCoursesList();
+  /**
+   * 受講生IDに紐づく受講生コース情報を検索します。
+   * @param studentId　受講生ID
+   * @return　受講生IDに紐づく受講生コース情報
+   */
   @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentsCourses> searchStudentsCourses(String studentId);
-
+  List<StudentsCourse> searchStudentsCourses(String studentId);
+  /**
+   * 受講生を新規登録します。IDに関しては自動採番を行う。
+   * @param student　受講生
+   */
   @Insert("INSERT INTO students(name, furigana, nickname, email, region, age, gender, remark, is_Deleted) "
       + "VALUES (#{name}, #{furigana}, #{nickname}, #{email}, #{region}, #{age}, #{gender}, #{remark}, false)")
   @Options(useGeneratedKeys = true,keyProperty = "id")
   void registerStudent(Student student);
-
+  /**
+   * 受講生コース情報を新規登録します。IDに関しては自動採番を行う。
+   * @param studentCourses　受講生コース情報
+   */
   @Insert("INSERT INTO students_courses(student_id, course_name, course_start_date, course_end_date) "
     + "VALUES(#{studentId}, #{courseName}, #{courseStartDate}, #{courseEndDate})")
   @Options(useGeneratedKeys = true,keyProperty = "id")
-  void registerStudentCourses(StudentsCourses studentCourses);
+  void registerStudentCourses(StudentsCourse studentCourses);
 
   @Update("UPDATE students SET name = #{name}, furigana = #{furigana}, nickname = #{nickname}, "
       + "email = #{email}, region = #{region}, age = #{age}, gender = #{gender}, "
@@ -40,6 +63,5 @@ public interface StudentRepository {
   void updateStudent(Student student);
 
   @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id}")
-  void updateStudentsCourses(StudentsCourses studentCourses);
-
+  void updateStudentsCourses(StudentsCourse studentCourses);
 }
